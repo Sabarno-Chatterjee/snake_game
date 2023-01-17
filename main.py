67f3f7075e282a97e512a79e343e3d3d2b53164d
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-17 12:23:57 +0530

    generates new snake

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from turtle import Turtle, Screen
 
-
 my_screen = Screen()
 my_screen.setup(width=600, height=600)
 my_screen.title("Feed the Snake")
 my_screen.bgcolor("black")
 
+snake_body = []
 
 
+def generate_snake(x_position):
+    for _ in range(3):
+        snake = Turtle(shape="square")
+        snake.color("white")
+        snake.shapesize()
+        snake.goto(x=x_position, y=0)
+        snake_body.append(snake)
+        x_position -= 20
 
+
+generate_snake(0)
 
 my_screen.exitonclick()
