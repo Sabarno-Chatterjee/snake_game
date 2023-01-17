200f21d6d7a7adf21f6d637d2403f5130cdf2d1f
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-17 13:14:27 +0530

    Snake movement

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from turtle import Turtle, Screen
+import time
 
 my_screen = Screen()
 my_screen.setup(width=600, height=600)
 my_screen.title("Feed the Snake")
 my_screen.bgcolor("black")
+my_screen.tracer(0)
 
 snake_body = []
 
 
-def generate_snake(x_position):
-    for _ in range(3):
+def generate_snake():
+    positions = [(0, 0), (-20, 0), (-40, 0)]
+    for position in positions:
         snake = Turtle(shape="square")
+        snake.speed(0)
         snake.color("white")
         snake.shapesize()
-        snake.goto(x=x_position, y=0)
+        snake.penup()
+        snake.goto(position)
         snake_body.append(snake)
-        x_position -= 20
 
 
-generate_snake(0)
+generate_snake()
+my_screen.update()
+game_is_on = True
+
+while game_is_on:
+    my_screen.update()
+    for snake in snake_body:
+        snake.forward(20)
+        time.sleep(0.1)
 
 my_screen.exitonclick()
