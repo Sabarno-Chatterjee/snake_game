8e0d06e66aa5d275733f487a8044704922346c58
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-17 12:04:27 +0530

    Added title to my_screen object

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+
+my_screen = Screen()
+my_screen.setup(width=600, height=600)
+my_screen.title("Feed the Snake")
+my_screen.bgcolor("black")
+
+
+
+
+
+my_screen.exitonclick()
