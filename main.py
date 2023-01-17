028cb2f30e7bbedf4229e268e142bd743f0b3342
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-17 15:17:33 +0530

    Edit main.py and snake.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-from snake import generate_snake, move
+from snake import Snake
 
 my_screen = Screen()
 my_screen.setup(width=600, height=600)
@@ -7,13 +7,13 @@ my_screen.title("Feed the Snake")
 my_screen.bgcolor("black")
 my_screen.tracer(0)
 
-generate_snake()
+snake = Snake()
 my_screen.update()
 game_is_on = True
 
 while game_is_on:
     my_screen.update()
-    move()
+    snake.move()
 
 
 my_screen.exitonclick()
